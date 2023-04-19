78f1fff2d714d349d9f7b2953472e84505bf6a7a
wayslow <120993943+wayslow@users.noreply.github.com>
2023-04-19 19:50:57 +0300

    реформировал код

--- main.py
+++ main.py
@@ -22,44 +22,33 @@ def check_for_redirect(response):
         raise requests.exceptions.HTTPError
 
 
-def get_book_name(domin, book_id):
-    path = f"/b{book_id}"
-    page_url = domin + path
-
-    page = get_page(page_url, )
-    soup = BeautifulSoup(page.text, 'lxml')
-
+def get_book_name(soup):
     title_tag = soup.find('h1')
     book_name = sanitize_filename("_".join(title_tag.text.split('   ::   ')[0].split(" ")).strip())
 
     book_name = check_same_name(book_name)
-    return book_name, page_url, soup
+
+    return book_name
 
 
-def download_photo_url(page_url, soup, filename, folder_name):
-    photo_url = urljoin(page_url, soup.find("div", class_="bookimage").find('img')['src'])
-
+def get_photo_url(page_url, soup, filename, folder_name):
     img = get_page(photo_url).content
     parse = urlsplit(photo_url)
     extension = os.path.splitext(parse.path)[-1]
 
     path = os.path.join(folder_name, f"{filename}{extension}")
+    return path
+    '''with open(path, 'wb') as file:
+        file.write(img)'''
 
-    with open(path, 'wb') as file:
-        file.write(img)
 
-
-def download_comments(soup):
+def get_comments(soup):
+    comments = []
     comments_tags = soup.find_all("span", class_="black")
     print(comments_tags)
     for coment_tag in comments_tags:
-        comment = coment_tag.text
-        print(comment)
-
-
-def download_genre(soup):
-    genre_tags = soup.find("div", id="content").find("a", href="/l55/")
-    print(genre_tags.text)
+        comments.append(coment_tag.text)
+    return comments
 
 
 def check_same_name(book_name):
@@ -76,6 +65,29 @@ def check_same_name(book_name):
     return verifiable_book_name
 
 
+def parse_book_page(book_id, domin, image_folder_name):
+    path = f"/b{book_id}"
+    page_url = domin + path
+
+    page = get_page(page_url, )
+    soup = BeautifulSoup(page.text, 'lxml')
+
+    book_name = get_book_name(soup)
+
+    photo_url = urljoin(page_url, soup.find("div", class_="bookimage").find('img')['src'])
+
+    genre = soup.find("span", class_="d_book").find("a").text
+
+    comments = get_comments(soup)
+    book_info = {
+        "book_name": book_name,
+        "photo_url": photo_url,
+        "genre": genre,
+        "comments": comments,
+
+    }
+
+
 def download_book(domin, book_id, books_folder_name, image_folder_name):
     params = {
         "id": book_id,
@@ -88,14 +100,6 @@ def download_book(domin, book_id, books_folder_name, image_folder_name):
     try:
         response = get_page(url, params=params)
         check_for_redirect(response)
-
-        book_name, page_url, soup = get_book_name(domin, book_id)
-
-        # download_photo_url(page_url, soup, book_name, image_folder_name)
-
-        #download_comments(soup)
-        download_genre(soup)
-
 
         books_path_file = os.path.join(books_folder_name, f"{book_name}.txt")
 
