1478a535b96e78b74be3ae1db492fd6051fdbe1b
wayslow <w12909900@gmail.com>
2023-08-01 15:27:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,36 +11,9 @@ from pathvalidate import sanitize_filename
 names_list = []
 
 
-def get_page(page_url, params=None):
-    response = requests.get(page_url, params)
-    response.raise_for_status()
-
-    return response
-
-
-def check_for_redirect(response):
-    if response.history:
-        raise requests.exceptions.HTTPError
-
-
-def get_photo_url(page_url, filename, folder_name):
-    img = get_page(page_url).content
-    parse = urlsplit(page_url)
-    extension = os.path.splitext(parse.path)[-1]
-
-    path = os.path.join(folder_name, f"{filename}{extension}")
-    with open(path, 'wb') as file:
-        file.write(img)
-    return path
-
-
-def get_comments(soup):
-    comments = []
-    comments_tags = soup.find_all("span", class_="black")
-    print(comments_tags)
-    for coment_tag in comments_tags:
-        comments.append(coment_tag.text)
-    return comments
+def download_book(book_text, books_path_file):
+    with open(books_path_file, "w", encoding="utf-8") as book:
+        book.write(book_text)
 
 
 def check_same_name(book_name):
@@ -56,81 +29,76 @@ def check_same_name(book_name):
     return verifiable_book_name
 
 
-def parse_book_page(book_id, domin, image_folder_name):
-    path = f"/b{book_id}"
-    page_url = domin + path
+def get_page(page_url, params=None):
+    response = requests.get(page_url, params)
+    response.raise_for_status()
+    return response
 
-    page = get_page(page_url, )
-    soup = BeautifulSoup(page.text, 'lxml')
+
+def check_for_redirect(response):
+    if response.history:
+        raise requests.exceptions.HTTPError
+
+
+def get_book_characteristic(soup, page_url):
+    book_image_div = soup.find("div", class_="bookimage")
+    book_image_div = book_image_div.find('img')['src']
+    photo_url = urljoin(page_url, book_image_div)
 
     title_tag = soup.find('h1')
-
     book_name = sanitize_filename("_".join(title_tag.text.split('   ::   ')[0].split(" ")).strip())
-
-    book_file_name = check_same_name(book_name)
-
-    photo_url = urljoin(page_url, soup.find("div", class_="bookimage").find('img')['src'])
-
-    genre = soup.find("span", class_="d_book").find("a").text
-
-    path_photo = get_photo_url(page_url, book_file_name, image_folder_name)
-
-    comments = get_comments(soup)
-
-    book_info = {
-        "book_name": book_name,
-        "book_file_name": book_file_name,
-        "photo_url": photo_url,
-        "path_photo":path_photo,
-        "genre": genre,
-        "comments": comments,
-    }
-
-    return book_info
+    file_name = check_same_name(book_name)
+    return file_name, photo_url
 
 
-def download_book(domin, book_id, books_folder_name, image_folder_name):
-    params = {
-        "id": book_id,
-    }
-
-    path = "/txt.php"
-
-    url = domin + path
-
-    try:
-        response = get_page(url, params=params)
-        check_for_redirect(response)
-
-        book_info = parse_book_page(book_id, domin, image_folder_name)
-
-        books_path_file = os.path.join(books_folder_name, f"{book_info['book_file_name']}.txt")
-
-        with open(books_path_file, "w", encoding="utf-8") as book:
-            book.write(response.text)
-
-    except requests.exceptions.HTTPError:
-        pass
+def download_img(path, img):
+    with open(path, 'wb') as file:
+        file.write(img)
 
 
 def main():
-    parser = argparse.ArgumentParser
-    parser.add_argument('start_id', help='начальное id')
-    parser.add_argument('end_id', help='конечное id')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--start_page', help='начальное id', default=1)
+    parser.add_argument('--end_page', help='конечное id', default=3)
     args = parser.parse_args()
-    start_id = args.start_id
-    end_id = args.end_id
+    start_id = int(args.start_page)
+    end_id = int(args.end_page)
 
     books_folder_name = "books"
     pathlib.Path(books_folder_name).mkdir(parents=True, exist_ok=True)
 
     image_folder_name = "imge"
     pathlib.Path(image_folder_name).mkdir(parents=True, exist_ok=True)
-
+    domin = 'https://tululu.org'
+    txt_path = "/txt.php"
     for book_id in range(start_id, end_id):
-        domin = 'https://tululu.org'
-        download_book(domin, book_id, books_folder_name, image_folder_name)
+        try:
+            params = {
+                "id": book_id,
+            }
+            book_text_url = urljoin(domin, txt_path)
+            book_text_page = get_page(book_text_url, params=params)
+            check_for_redirect(book_text_page)
+            book_text = book_text_page.text
 
+            page_url = domin + f"/b{book_id}"
+            book_page = get_page(page_url)
+            soup = BeautifulSoup(book_page.text, 'lxml')
+            file_name, photo_url = get_book_characteristic(soup, page_url)
+
+            books_path_file = os.path.join(books_folder_name, f"{file_name}.txt")
+
+            img = get_page(photo_url).content
+            parse = urlsplit(photo_url)
+            extension = os.path.splitext(parse.path)[-1]
+
+            img_path_file = os.path.join(image_folder_name, f"{file_name}{extension}")
+
+            download_book(book_text, books_path_file)
+            download_img(img_path_file, img)
+
+        except requests.exceptions.HTTPError:
+            pass
 
 if __name__ == '__main__':
     main()
