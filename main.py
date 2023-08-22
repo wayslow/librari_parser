5a12179c99f196ba16d85e8272f81c24b7f88104
wayslow <w12909900@gmail.com>
2023-08-22 15:12:28 +0300

    Format the code

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def download_book(book_text, books_path_file):
 def check_same_name(book_name, names, index=0):
     if book_name in names:
         book_name = f"{book_name}_{str(index)}"
-        check_same_name(book_name, names, index + 1)
+        check_same_name(book_name, names, index+1)
     else:
         names.append(book_name)
 
@@ -79,33 +79,32 @@ def main():
     txt_path = "/txt.php"
     names = []
     for book_id in range(start_id, end_id):
-            try:
-                params = {
-                    "id": book_id,
-                }
-                book_text_url = urljoin(library_domin, txt_path, )
-                book_text_page = get_page(book_text_url, params=params)
+        try:
+            params = {
+                "id": book_id,
+            }
+            book_text_url = urljoin(library_domin, txt_path)
+            book_text_page = get_page(book_text_url, params=params)
 
-                book_text = book_text_page.text
+            book_text = book_text_page.text
 
-                page_url = f"{library_domin}/b{book_id}"
-                book_page = get_page(page_url)
-                soup = BeautifulSoup(book_page.text, 'lxml')
-                file_name, photo_url, genre, author = parse_book_page(soup, page_url, names)
+            page_url = f"{library_domin}/b{book_id}"
+            book_page = get_page(page_url)
+            soup = BeautifulSoup(book_page.text, 'lxml')
+            file_name, photo_url, genre, author = parse_book_page(soup, page_url, names)
 
-                books_path_file = os.path.join(books_folder_name, f"{file_name}.txt")
+            books_path_file = os.path.join(books_folder_name, f"{file_name}.txt")
 
-                img = get_page(photo_url).content
-                parse = urlsplit(photo_url)
-                extension = os.path.splitext(parse.path)[-1]
+            img = get_page(photo_url).content
+            parse = urlsplit(photo_url)
+            extension = os.path.splitext(parse.path)[-1]
 
-                img_path_file = os.path.join(image_folder_name, f"{file_name}{extension}")
+            img_path_file = os.path.join(image_folder_name, f"{file_name}{extension}")
 
-                download_book(book_text, books_path_file)
-                download_img(img_path_file, img)
-            except requests.exceptions.HTTPError:
-                print("requests.exceptions.HTTPError")
-
+            download_book(book_text, books_path_file)
+            download_img(img_path_file, img)
+        except requests.exceptions.HTTPError:
+            print("requests.exceptions.HTTPError")
 
 
 if __name__ == '__main__':
