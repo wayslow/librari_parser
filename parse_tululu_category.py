3b49bed584dc6a25a722df4a3bcbc9a718fc09cb
wayslow <w12909900@gmail.com>
2023-09-23 15:03:47 +0300

    Update parse_tululu_category.py

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -15,7 +15,9 @@ LIBRARY_DOMIN = 'https://tululu.org'
 TXT_PATH = "/txt.php"
 
 
-def parse_book_page(page_url):
+def get_book_properties(book_id):
+    page_url = f"{LIBRARY_DOMIN}/b{book_id}/"
+
     book_page = get_page(page_url)
     soup = BeautifulSoup(book_page.text, 'lxml')
 
@@ -44,7 +46,7 @@ def parse_book_page(page_url):
     return book_properties
 
 
-def image_downloud(book_properties, image_folder_name, file_name):
+def image_download(book_properties, image_folder_name, file_name):
     photo_url = book_properties["photo_url"]
     img = get_page(photo_url).content
     parse = urlsplit(photo_url)
@@ -57,7 +59,7 @@ def image_downloud(book_properties, image_folder_name, file_name):
     return book_properties
 
 
-def book_downloud(book_id, books_folder_name, book_properties, file_name):
+def book_download(book_id, books_folder_name, book_properties, file_name):
     params = {
         "id": book_id,
     }
@@ -74,64 +76,71 @@ def book_downloud(book_id, books_folder_name, book_properties, file_name):
     return book_properties
 
 
-def maker_json(books_properties, file_path):
-    with open(file_path, encoding="utf-8") as file:
-        in_file_properties = json.load(file)
+def make_json(books_properties, file_path):
+    try:
+        with open(file_path, encoding="utf-8") as file:
+            in_file_properties = json.load(file)
+            books_properties = books_properties | in_file_properties
 
-    books_properties = books_properties | in_file_properties
+    except (json.decoder.JSONDecodeError, FileNotFoundError):
+        pass
 
     with open(file_path, 'w', encoding="utf-8") as file:
         json.dump(books_properties, file, indent=4, ensure_ascii=False)
 
 
 def find_end_id(category_url):
-    category_page = get_page(category_url).text
+    category_page = get_page(category_url).text  # поучение страницы
     soup = BeautifulSoup(category_page, "lxml")
-    end_id = soup.select("a.npage")[-1].text
+    end_id = soup.select("a.npage")[-1].text  # нахождение последнего айди страницы из блока
     return int(end_id)
 
 
 def pars_category(category_url, books_folder_name, image_folder_name, skip_txt, skip_img, json_file_path):
     books_properties = {}
     try:
-        category_page = get_page(category_url).text
 
-        soup = BeautifulSoup(category_page, "lxml")
+        category_page = get_page(category_url).text  # получаем страницу
 
-        books_table = soup.select("table.d_book")
+        soup = BeautifulSoup(category_page, "lxml")  # переводим в объект супа
 
-        for book in books_table:
+        books_table = soup.select("table.d_book")  # получаем блок с книгами
+
+        for book in books_table:  # парсим страницу и получаем book_properties
             book_id = book.select("a")[0]["href"][2:-1]
-            page_url = f"{LIBRARY_DOMIN}/b{book_id}/"
-            book_properties = parse_book_page(page_url)
+
+            book_properties = get_book_properties(book_id)
+
             file_name = book_properties["file_name"]
-            if skip_txt:
-                book_properties = book_downloud(book_id, books_folder_name, book_properties, file_name)
-            if skip_img:
-                book_properties = image_downloud(book_properties, image_folder_name, file_name)
+            if not skip_txt:
+                book_properties = book_download(book_id, books_folder_name, book_properties, file_name)
+            if not skip_img:
+                book_properties = image_download(book_properties, image_folder_name, file_name)
 
             books_properties[book_id] = book_properties
 
-    except requests.exceptions.HTTPError:
-        print("requests.exceptions.HTTPError")
+    except (requests.exceptions.HTTPError, ConnectionError) as exceptions:
+        print("проблемы с соединенем")
     except KeyboardInterrupt:
-        print("переже временное откючение")
+        print("преждевременное отключение")
         exit()
     except PageDontExist:
-        print("некой книги нет")
+        print("некой страницы нет")
     finally:
-        maker_json(books_properties, json_file_path)
+        make_json(books_properties, json_file_path)
 
 
 def main():
+    # создание аргументов
     parser = argparse.ArgumentParser(description="скачивает книги по id с онлайн библиотеки tululu.org")
-    parser.add_argument('--start_page', help='начальное страницы id', default=1, type=int)
-    parser.add_argument('--end_page', help='конечное страницы id', default=None, type=int)
+    parser.add_argument('--start_page', help='начальное id страницы', default=1, type=int)
+    parser.add_argument('--end_page', help='конечное id страницы', default=None, type=int)
     parser.add_argument('--category_id', help='id котегории', default=55, type=int)
     parser.add_argument('--dest_folder', help='путь к папке сохранеия', default="", type=str)
     parser.add_argument('--skip_img', help=' сохранять изображение', action="store_true")
     parser.add_argument('--skip_txt', help=' сохранять текст', action="store_true")
 
+    # парсинг аргументов
     args = parser.parse_args()
     start_id = args.start_page
     end_id = args.end_page
@@ -139,27 +148,31 @@ def main():
     dest_folder = args.dest_folder
     skip_img = args.skip_img
     skip_txt = args.skip_txt
+
+    # создание папок при надобности
     books_folder_name = "books"
-    if skip_txt:
+    if not skip_txt:
         books_folder_name = os.path.join(dest_folder, books_folder_name)
         pathlib.Path(books_folder_name).mkdir(parents=True, exist_ok=True)
 
     image_folder_name = "imge"
-    if skip_img:
+    if not skip_img:
         image_folder_name = os.path.join(dest_folder, image_folder_name)
         pathlib.Path(image_folder_name).mkdir(parents=True, exist_ok=True)
 
     pathlib.Path(dest_folder).mkdir(parents=True, exist_ok=True)
-
     json_file_path = os.path.join(dest_folder, "book_properties.json")
+
+    # удаление преведущего фала "book_properties.json" при его наличии
     pathlib.Path(json_file_path).unlink(missing_ok=True)
-    open(json_file_path, 'w', encoding="utf-8")
 
     category_url = f"{LIBRARY_DOMIN}/l{category_id}/"
 
     if not end_id:
+        # нахождение end_id
         end_id = find_end_id(category_url)
 
+    # проход по всем страницам от start_id до end_id
     for page_id in range(start_id, end_id):
         category_page_url = f"{category_url}{page_id}/"
 
